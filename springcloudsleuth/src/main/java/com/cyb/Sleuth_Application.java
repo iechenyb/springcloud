5c33e91b914fa1d244dc70ed4310f799ad6dd394
iechenyb <zzuchenyb@sina.com>
2017-11-06 21:29:54 +0800

    session共享

--- springcloudsleuth/src/main/java/com/cyb/Sleuth_Application.java
+++ springcloudsleuth/src/main/java/com/cyb/Sleuth_Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import zipkin.server.EnableZipkinServer;
-
+//https://yq.aliyun.com/articles/78128?utm_campaign=wenzhang&utm_medium=article&utm_source=QQ-qun&201758&utm_content=m_19862
 @SpringBootApplication
 @EnableZipkinServer
 public class Sleuth_Application {
