c3dac83f8b08a7a6e03ac615c855a2cb96838001
iechenyb <zzuchenyb@sina.com>
2018-12-14 17:12:50 +0800

    代码整理

--- springcloudconfigclient/src/main/java/com/cyb/ConfigClientApplication.java
+++ springcloudconfigclient/src/main/java/com/cyb/ConfigClientApplication.java
@@ -3,28 +3,33 @@ package com.cyb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
-
-/**
- * @author Administrator
- *http://blog.csdn.net/gongxsh00/article/details/51292200\
- *启动注意事项 -Dspring.profiles.active=test   test为文件后缀名，
- *bootstrap.yml 中的spring:application:name=cyb,最终加载的文件为git/cyb-test.yml
- * http://localhost:7776/my-client/master
- *从git刷新配置信息 http://localhost:7776/refresh  仅仅支持post请求！
- *不走消息总线的时候，需要通知各个客户端进行刷新。
- */
 @SpringBootApplication
 public class ConfigClientApplication  {
 	public static void main(String[] args) {
-		//SpringApplication.run(ConfigClientApplication.class, args);
-		//spring.application.name+test.properties
-		new SpringApplicationBuilder(ConfigClientApplication.class).web(true).profiles("dev").run(args);
+		SpringApplication.
+		run(ConfigClientApplication.class, args);
 	}
      //项目启动时，读取配置文件参数做初始化使用
 	@Autowired
 	void setEnvironment(Environment env) {
-		System.out.println("param from env: " + env.getProperty("from"));
+		System.out.println("param from env: "
+				+ env.getProperty("from"));
+	}
+	/**
+	 * ${from} 不存在时 直接将其作为值
+	 * 设置@Value注解取值不到忽略（不报错）
+	 *作者 : iechenyb<br>
+	 *方法描述: 说点啥<br>
+	 *创建时间: 2017年7月15日
+	 *@return
+	 */
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+	    PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+	    c.setIgnoreUnresolvablePlaceholders(true);
+	    return c;
 	}
 }
--- springcloudconfigclient/src/main/java/com/cyb/controller/ParameterRestController.java
+++ springcloudconfigclient/src/main/java/com/cyb/controller/ParameterRestController.java
@@ -1,44 +1,23 @@
 package com.cyb.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /*
- * http://localhost:7776/git/param/from gith上获取from的属性值
- * http://localhost:7776/local/el/name 从本地获取el注入的属性值
- * http://localhost:7776/local/fel/name 从本地获取非el注入的属性值（没调试通）
- * springcloud Could not resolve placeholder 'from' in string value "${from}"
- * 解决方案，请看启动类
+     解决方案，请看启动类
  */
 @RestController
-@RefreshScope// 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
+@RefreshScope
+/* 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，
+ * 自动将新的配置更新到该类对应的字段中。*/
 public class ParameterRestController {
 	
-	@Autowired
-	ELConfig elConfig;
-	
-	@Autowired
-	AuthorSettings setting;
-	
-	//git
 	@Value("${from}")
 	private String from;
 	
-	//从git上读取from的属性值
-	@RequestMapping("/git/param/from")
-	public String getGitFrom() {
+	@RequestMapping("getConfigFromConfigServer")
+	public String getConfigFromConfigServer() {
 		return from;
-	}
-	
-	@RequestMapping("/local/el/name")
-	public String getLocal1() {
-		return elConfig.getName();
-	}
-	
-	@RequestMapping("/local/fel/name")
-	public String getLocal2() {
-		return setting.getName();
 	}
 }
