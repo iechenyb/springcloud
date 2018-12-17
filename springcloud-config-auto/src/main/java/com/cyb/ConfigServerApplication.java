576f20ff0a575e7c4dc7771edec92bd3af5f76b6
iechenyb <zzuchenyb@sina.com>
2018-12-17 09:22:38 +0800

    提交

--- springcloud-config-auto/src/main/java/com/cyb/ConfigServerApplication.java
+++ springcloud-config-auto/src/main/java/com/cyb/ConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.cyb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableConfigServer
+@EnableDiscoveryClient
 @RestController
 public class ConfigServerApplication {
 	@RequestMapping("/")
--- springcloudconfigclient/src/main/java/com/cyb/ConfigClientApplication.java
+++ springcloudconfigclient/src/main/java/com/cyb/ConfigClientApplication.java
@@ -3,10 +3,12 @@ package com.cyb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication  {
 	public static void main(String[] args) {
 		SpringApplication.
